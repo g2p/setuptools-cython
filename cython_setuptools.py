ecd2bee3d96069dae6967e40d327b2de904c379f
Gabriel <g2p.code@gmail.com>
2009-06-05 12:52:50 +0200

    Works, although quite dirty.

--- cython_setuptools.py
+++ cython_setuptools.py
@@ -1,8 +1,16 @@
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
 import Cython.Distutils.build_ext
 
+def ext_modules_hack(dist, attr, value):
+    dist.cmdclass['build_ext'] = Cython.Distutils.build_ext
+
 def validate_cython_ext_modules(dist, attr, value):
     build_cython(dist).check_extensions_list(value)
+    build_cls = dist.get_command_class('build')
+    sc = dict(build_cls.sub_commands)
+    if 'build_cython' not in sc:
+        sc['build_cython'] = None
+        build_cls.sub_commands = sc.items()
 
 class build_cython(Cython.Distutils.build_ext, object):
     def initialize_options(self):
--- setup.py
+++ setup.py
@@ -2,14 +2,48 @@
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
 from setuptools import setup
 
-# setuptools DWIM madness:
-# http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
-# No way but to unpatch, by copying the following code in individual scripts.
+"""
+Usage:
+Use setuptools and put cython_setuptools in your setup_requires.
+Also paste the monkey-patch reversal below.
+
+setuptools DWIM monkey-patch madness
+http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
+
+No way but to unpatch, by copying the following code in individual setup scripts:
+
 import sys
 if 'setuptools.extension' in sys.modules:
     m = sys.modules['setuptools.extension']
     m.Extension.__dict__ = m._Extension.__dict__
 
+Complete example:
+
+#!/usr/bin/env python
+
+from setuptools import setup
+from distutils.extension import Extension
+
+# setuptools DWIM monkey-patch madness
+# http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
+import sys
+if 'setuptools.extension' in sys.modules:
+    m = sys.modules['setuptools.extension']
+    m.Extension.__dict__ = m._Extension.__dict__
+
+setup(
+        name = "example",
+        version = "0.1",
+        description="cython_setuptools example",
+        setup_requires=[
+            'cython_setuptools',
+            ],
+        ext_modules=[
+            Extension('example', ['example.pyx']),
+            ],
+        )
+
+"""
 
 setup(
         name='cython-setuptools',
@@ -21,9 +55,6 @@ setup(
         long_description='Allows using Cython in setuptools projects\n'
         +'by putting cython-setuptools in your setup_requires.\n',
         py_modules=['cython_setuptools', ],
-        setup_requires=[
-            'Cython',
-            ],
         install_requires=[
             'Cython',
             ],
@@ -40,20 +71,24 @@ setup(
             # when they are on the command line.
 
             # This means distutils integration using distutils extension points
-            # is an impossibility. Only monkey-patching works, and cython-setuptools
-            # is useless.
+            # is an impossibility.
+            # But we have our own entry point!
+            # Take advantage of the parameter validation.
+            # In fact, we can write a 'validation' for the original ext_modules
+            # parameter.
 
             'distutils.commands': [
-                'build_cython = cython_setuptools:build_cython',
-                'build_ext_cython = Cython.Distutils:build_ext',
+                #'build_cython = cython_setuptools:build_cython',
                 ],
             'distutils.setup_keywords': [
-                'cython_ext_modules = cython_setuptools:validate_cython_ext_modules',
+                'ext_modules = cython_setuptools:ext_modules_hack',
+                #'cython_ext_modules = cython_setuptools:validate_cython_ext_modules',
                 ],
 
             },
         classifiers=[
             'Framework :: Setuptools Plugin',
+            'Topic :: Software Development :: Build Tools',
             ],
         )
 
